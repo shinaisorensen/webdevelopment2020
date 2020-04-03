5d95935607d88d544540feab0e72980a5ca9f85c
SSorensen <43558696+shinaisorensen@users.noreply.github.com>
2020-04-03 01:24:16 -0700

    Added BCIT Winter Term HTML Final Project

---
+++ BCIT Class Web Projects Final Project - 2020/header.php
@@ -0,0 +1,21 @@
+<!-- 
+    Author: Shinai Sorensen
+    Date: March 29, 2020
+ -->
+
+<!-- Header -->
+<header class='header-grid-container'>
+
+    <!-- BCIT Logo -->
+    <figure class='header-grid-item'>
+        <a href='http://bcitcomp.ca/'>
+            <img src='/students/ssorensen23/images/bcitLogo.jpg' alt='Return to Course Selection'>
+        </a>
+    </figure>
+
+    <!-- Title of content -->
+    <div class='header-grid-item'>
+        <h1>COMP 1950 - Web Development and Design 2</h1>
+        <p>Monday / Wednesday</p>
+    </div>
+</header>
---
+++ BCIT Class Web Projects Final Project - 2020/navigation.php
@@ -0,0 +1,26 @@
+<!-- Navigation Bar -->
+<nav>
+    <!-- Title of Nav Bar -->
+    <h2><a href='/students/ssorensen23/homePage.html'>Home</a></h2>
+
+    <ul>
+        <!-- Lectures -->
+        <li><a href='/students/ssorensen23/lecture.html'>Lecture 1: Tools and Standards</a></li>
+        <li><a href='#'>Lecture 2: HTML5</a></li>
+        <li><a href='#'>Lecture 3: Advanced CSS</a></li>
+        <li><a href='#'>Lecture 4: CSS Layouts</a></li>
+        <li><a href='#'>Lecture 5: Responsive Design</a></li>
+        <li><a href='#'>Lecture 6: CSS3</a></li>
+        <li><a href='#'>Lecture 7: Javascript Frameworks</a></li>
+        <li><a href='#'>Lecture 8: Server Architecture</a></li>
+        <li><a href='#'>Lecture 9: Templates, Tools</a></li>
+        <li><a href='#'>Lecture 10: Methodology</a></li>        
+
+        <!-- Resources and Other Links -->
+        <li><a href='#'>Resources</a></li>
+        <li><a href='/students/ssorensen23/about.html'>About</a></li>
+        <li><a href='#'>BCIT</a></li>
+        <li><a href='#'>myBCIT</a></li>
+        <li><a href='#'>Learning Hub</a></li>
+    </ul>
+</nav>
